de7498b7bbc2e654a09a978c1382c2f13717e094
Strepped <bel.88.07@gmail.com>
2017-04-07 13:47:35 +1000

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+ * Class Почти калькулятор.
+ * @author Yury Vlasov
+ * @since 07.04.2017
+ * @version 1.0
+*/
 public class Calculate {
-	public static void main(String[] args) {
-		System.out.println("Hello world.");
-	}
+    /**
+    * Метод, выводит сроку в консоль.
+    * @param args - args.
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
 }
