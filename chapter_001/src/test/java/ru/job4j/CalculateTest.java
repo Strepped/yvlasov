e73d686d79df4da7c3f79e4874a39bc46563aa6d
Strepped <bel.88.07@gmail.com>
2017-04-07 15:17:56 +1000

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Yury Vlasov (mailto:bel.88.07@gmail.com)
+* @version 1.0
+* @since 01.04.2017
+*/
+public class CalculateTest {
+/**
+* Test add.
+*/
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello World%s",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
+    }
+} 
