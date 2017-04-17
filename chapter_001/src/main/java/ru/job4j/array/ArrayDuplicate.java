02b4aa2489dc5055579260aa72a8268ad696873b
Strepped <bel.88.07@gmail.com>
2017-04-17 18:04:19 +1000

    task 225

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -18,16 +18,16 @@ public class ArrayDuplicate {
 	*/
 
 	public String[] remove(String[] array) {
-		int count = 0;
-		for (int i = 0; i < array.length - 1; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				if (array[i].equals(array[j])) {
-					array[j] = array[array.length - 1];
-					j--;
-					count++;
+		int count = array.length;
+			for (int i = 0; i < count; i++) {
+				for (int j = i + 1; j < count; j++) {
+					if (array[i].equals(array[j])) {
+						array[j] = array[count - 1];
+						j--;
+						count--;
+					}
 				}
 			}
-		}
-	return Arrays.copyOf(array, array.length - count);
+	return Arrays.copyOf(array, count);
 	}
 }
